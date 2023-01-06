783106c34f461170a3f4b8c9521caa918f81bce2
Manikandan R <52978899+mani2care@users.noreply.github.com>
2023-01-06 21:23:36 +0530

    Add files via upload

---
+++ macos_Installer_check.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+###############################################################
+#Script is checking for mac os Installercheck 
+###############################################################
+search_path="/Applications/"
+test=$(ls /Applications/ | grep "Install macOS*")
+if [[ $test == "" ]]; then
+    echo "<result>No-Installer</result>" 
+else
+    ls /Applications/ | grep "Install macOS*"|
+while IFS= read -r line
+do 
+    echo "<result>$line</result>"                  
+done
+fi

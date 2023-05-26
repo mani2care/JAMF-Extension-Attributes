4424d44c99cb7cdff5c2b17bc7c38043011154ba
Manikandan R <52978899+mani2care@users.noreply.github.com>
2023-05-26 20:33:42 +0530

    Update EA_Remote-Login_Remote-Management_status-check.sh

--- EA_Remote-Login_Remote-Management_status-check.sh
+++ EA_Remote-Login_Remote-Management_status-check.sh
@@ -3,15 +3,15 @@
 sudo launchctl list com.openssh.sshd &> /dev/null;
 if [ $? -eq 0 ]
 then
-  echo "Remote_Login_Enabled"
+  echo "<result>Remote_Login_Enabled</result>"
 else
-  echo "Remote_Login_Disabled"
+  echo "<result>Remote_Login_Disabled</result>"
 fi
 
 # Check if remote management is enabled
 agent_process=$(pgrep -x ARDAgent)
 if [ -n "$agent_process" ]; then
-    echo "Remote_Management_Enabled"
+    echo "<result>Remote_Management_Enabled</result>"
 else
-    echo "Remote_Management_Disabled"
+    echo "<result>Remote_Management_Disabled</result>"
 fi

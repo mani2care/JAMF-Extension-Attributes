96d56951364171595a2db336273470fd0d44efbd
Manikandan R <52978899+mani2care@users.noreply.github.com>
2023-11-01 11:38:41 +0530

    Update List All Installed Apps.s

--- List All Installed Apps.s
+++ List All Installed Apps.s
@@ -1,14 +1,8 @@
 #!/bin/zsh
-#######################
-#
 # List_All_Installed_Apps.sh v1.2
 #
 # Finds and lists all installed applications on a Mac.
 #       Short script to list all .app bundles on a Mac using Spotlight to search the contents of /Applications/.
 #       Apple applications are excluded using the com.apple.* bundle identifier.
-# Greg Knackstedt
-# 6.10.2022
-#
-######################
 #
 mdfind -onlyin /Applications/ '(kMDItemCFBundleIdentifier != "com.apple.*" && kMDItemKind == "Application")' | sort -g

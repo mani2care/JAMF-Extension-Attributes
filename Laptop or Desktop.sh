bb0e764154620447503fd556cac6776f91e1c128
Manikandan R <52978899+mani2care@users.noreply.github.com>
2023-11-01 13:24:23 +0530

    Create Laptop or Desktop.sh

---
+++ Laptop or Desktop.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Script tested as working in bash + zsh
+####################################################################
+# HardwareType_isLaptop
+# Extension attribute to identify if a system is a laptop or desktop by detecting the presence of a battery.
+# Value will be "Yes" if system is a laptop or "No" if the system is a desktop. 
+####################################################################
+#
+# Check if system has an internal battery
+isLaptop=$(/usr/sbin/ioreg -c AppleSmartBattery -r | awk '/BatteryInstalled/ {print $3}')
+#
+# If system is a laptop, set result to Yes
+if [[ $isLaptop == "Yes" ]]; then
+	echo "System is a laptop"
+ 	result="Yes"
+else
+# Else system is a desktop, set result to No
+	echo "System is a desktop"
+	result="No"
+fi
+# Echo the result for collection
+/bin/echo "<result>$result</result>"

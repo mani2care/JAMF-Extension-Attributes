464ad5d6b4596d73b2e44a226f41fbdf967d7bbc
Manikandan R <52978899+mani2care@users.noreply.github.com>
2023-11-01 11:39:01 +0530

    Update List All Installed Apps Except Adobe.sh

--- List All Installed Apps Except Adobe.sh
+++ List All Installed Apps Except Adobe.sh
@@ -1,15 +1,9 @@
 #!/bin/zsh
-#######################
-#
 # List_All_Installed_Apps_Except_Adobe.sh v1.3
 #
 # Finds and lists all installed applications on a Mac.
 #       Short script to list all .app bundles on a Mac using Spotlight to search the contents of /Applications/.
 #       Apple applications are excluded using the com.apple.* bundle identifier.
 #       Adobe applications are excluded using the com.adobe.* bundle identifier.
-# Greg Knackstedt
-# 6.10.2022
-#
-######################
 #
 mdfind -onlyin /Applications/ '(kMDItemCFBundleIdentifier != "com.apple.*" && kMDItemCFBundleIdentifier != "com.adobe.*") && kMDItemKind == "Application"' | sort -g

27cfd7f4390d40e7a6a387400346819bf21caaa8
Manikandan R <52978899+mani2care@users.noreply.github.com>
2023-11-11 02:36:51 +0530

    Create User All Matching Certificate Name+Expiration Date(Expiring in 30 days).sh

---
+++ Certificate/User All Matching Certificate Name+Expiration Date(Expiring in 30 days).sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Grabs all certificate information
+cert_info=$(security find-identity | grep -v expired | awk -F '"' '/./ {print $2}' | awk 'NF')
+
+# Check each certificate
+IFS=$'\n'
+output=""
+current_date=$(date +"%s")
+threshold_days=30
+
+for CompName in $cert_info; do
+    ExpDate=$(/usr/bin/security find-certificate -a -c "$CompName" -p -Z | sed -n 'H; /^SHA-256/h; ${g;p;}' | /usr/bin/openssl x509 -noout -enddate 2>/dev/null | cut -f2 -d= | xargs -I {} sh -c 'if [ -n "{}" ]; then date -jf "%b %e %T %Y %Z" "{}" +"%e-%b-%Y"; fi')
+    
+    if [ -z "$ExpDate" ]; then
+        result="Not_expired"
+    else
+        exp_date_unix=$(date -j -f "%d-%b-%Y" "$ExpDate" +"%s")
+        days_until_expiry=$(( ($exp_date_unix - $current_date) / 86400 ))
+
+        if [ "$days_until_expiry" -ge 0 ] && [ "$days_until_expiry" -le "$threshold_days" ]; then
+            result="$CompName=$ExpDate (+$days_until_expiry days)"
+            output="$output$result\n"
+        fi
+    fi
+done
+
+echo -e "<result>$output</result>"
+exit 0 # Success

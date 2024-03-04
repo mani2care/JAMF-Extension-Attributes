d38dfdd56a63933060b93e4ad89db0ea24f8cbc3
Manikandan R <52978899+mani2care@users.noreply.github.com>
2024-03-04 19:15:27 +0530

    Update TrendMicro version.sh

--- TrendMicro version.sh
+++ TrendMicro version.sh
@@ -2,10 +2,10 @@
 # Returns the "Product Version" of the installed client, if found
 
 
-if [ -d /Applications/Utilities/Nudge.app ]; then
-    AppVersion=$(defaults read /Applications/Utilities/Nudge.app/Contents/Info.plist CFBundleShortVersionString)
+if [ -d /Applications/TrendMicroSecurity.app ]; then
+    AppVersion=$(defaults read /Applications/TrendMicroSecurity.app/Contents/Info.plist CFBundleShortVersionString)
     echo "<result>$AppVersion</result>"
 else
     echo "<result>Not-installed</result>"
 fi
-exit
+exit 0

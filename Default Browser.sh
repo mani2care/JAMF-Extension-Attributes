0b6317d99654d79cb0ea6e0ac2ca2d7c36698766
Manikandan R <52978899+mani2care@users.noreply.github.com>
2024-03-14 19:04:11 +0530

    Create Default Browser.sh

---
+++ Default Browser.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+###
+#
+#            Name:  Default Browser.sh
+#     Description:  Returns default browser of currently logged-in user.
+#         Created:  2016-06-06
+#   Last Modified:  2023-03-13
+#         Version:  1.4
+
+########## variable-ing ##########
+
+
+
+loggedInUser=$(/usr/bin/stat -f%Su "/dev/console")
+loggedInUserHome=$(/usr/bin/dscl . -read "/Users/${loggedInUser}" NFSHomeDirectory | /usr/bin/awk '{print $NF}')
+defaultBrowser=$(/usr/bin/defaults read "${loggedInUserHome}/Library/Preferences/com.apple.LaunchServices/com.apple.launchservices.secure.plist" LSHandlers | /usr/bin/grep -B1 https | /usr/bin/awk -F\" '/LSHandlerRoleAll/ {print $2}' 2>"/dev/null")
+
+
+
+########## main process ##########
+
+
+
+# Report result.
+echo "<result>${defaultBrowser}</result>"
+
+
+
+exit 0

5aaaf990ba5bf621da1257d2a6ca0e48438e8ff7
Manikandan R <52978899+mani2care@users.noreply.github.com>
2024-11-12 14:39:30 +0530

    Update QualysCloudAgent version.sh

--- QualysCloudAgent version.sh
+++ QualysCloudAgent version.sh
@@ -3,9 +3,10 @@
 #Script is checking for QualysCloudAgent 
 ###############################################################
 if [ -d /Applications/QualysCloudAgent.app ]; then 
-CylanceVersion=$(/usr/bin/defaults read /Applications/QualysCloudAgent.app/Contents/Info.plist CFBundleShortVersionString)
-echo "<result>$CylanceVersion</result>" 
+    CylanceVersion=$(/usr/bin/defaults read /Applications/QualysCloudAgent.app/Contents/Info.plist CFBundleShortVersionString)
+    # Extract only the version number before the hyphen
+    CylanceVersion=$(echo "$CylanceVersion" | cut -d '-' -f 1)
+    echo "<result>$CylanceVersion</result>" 
 else 
-	echo "<result>Not-Installed</result>" 
-fi 
-exit 0
+    echo "<result>Not-Installed</result>" 
+fi
